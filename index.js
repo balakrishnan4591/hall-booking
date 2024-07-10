3b04e3c7f06f6bc62166a38b25d6b330f9bbcfd6
balakrishnan4591 <balakrishnan4591@gmail.com>
2024-07-10 13:55:35 +0530

    added few cosmetic changes

--- index.js
+++ index.js
@@ -23,37 +23,64 @@ app.use(express.json());
 app.get("/", (req, res) => {
   const htmlResponse = `
     <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hall Booking API</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f0f0f0;
-                margin: 0;
-                padding: 0;
-            }
-            .container {
-                text-align: center;
-                margin-top: 100px;
-            }
-            h1 {
-                color: #333;
-            }
-            p {
-                color: #666;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Hall Booking API Running Successfully</h1>
-            <p>To test the same do check the API in Postman.</p>
-        </div>
-    </body>
-    </html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hall Booking API</title>
+    <style>
+      body {
+        background-image: url("https://imageio.forbes.com/specials-images/imageserve/652f603a91415a3d647fc207/Modern-Style-Bedroom/960x0.jpg?format=jpg&width=960");
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        background-color: #e5eead;
+        margin: 0;
+        padding: 0;
+        display: grid;
+        place-items: center;
+      }
+      .container {
+        height: 200px;
+        width: 600px;
+
+        border: 2px solid rgb(77, 84, 79);
+        border-radius: 10px;
+
+        background: rgba(44, 42, 42, 0.8);
+        backdrop-filter: blur(10px);
+
+        margin-top: 100px;
+        text-align: center;
+      }
+      h1 {
+        color: #ffffff;
+      }
+      p {
+        color: #ffffff;
+      }
+      a {
+        color: rgb(96, 96, 221);
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Hall Booking API Running Successfully</h1>
+      <p>
+        To test the same
+        <a
+          href="https://documenter.getpostman.com/view/36588127/2sA3e48TPg
+      "
+          >Click here</a
+        >
+      </p>
+    </div>
+  </body>
+</html>
+
     `;
   res.status(200).send(htmlResponse);
 });
